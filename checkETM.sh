b8773c671c9159737eaa14369d9e22181324c3a6
EduJGURJC <ejg.urjc@gmail.com>
2017-09-25 16:52:32 +0200

    trying show check logs with echo connecting

--- checkETM.sh
+++ checkETM.sh
@@ -31,6 +31,7 @@ while ! nc -z -v $ET_ETM_API 8091 2> /dev/null; do
 	    echo ''
 	    echo "ETM is not ready. Please wait..."
     fi
+    echo 'connecting...'
     sleep 2
     # prevent infinite loop
     counter=$(($counter-1))
