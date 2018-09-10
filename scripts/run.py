fb9aa7fffed8ec6d7acbb48ec595cb27d5e98dc7
edujgurjc <26252085+EduJGURJC@users.noreply.github.com>
2018-09-10 09:36:52 +0200

    Bind testlink and jenkins ports

--- scripts/run.py
+++ scripts/run.py
@@ -154,7 +154,8 @@ def runPlatform(params):
         files_list = []
         if(mode == 'experimental'):            
             dockerCommand = 'docker-compose ' + platform_services + ' ' + edm + ' ' + etm + ' ' + esm + ' ' + eim + \
-                            ' ' + epm + ' ' + emp + ' ' + etm_proxy + ' ' + etm_tlink + ' ' + etm_jenkins + ' '
+                            ' ' + epm + ' ' + emp + ' ' + etm_proxy + ' ' + etm_tlink + ' ' + etm_tlink_ports + \ 
+                            ' ' + etm_jenkins + ' ' + etm_jenkins_ports + ' '
             
             #Replace emp env variables
             et_host = "localhost"
