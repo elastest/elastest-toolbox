134e575c694e0bd249a1c68c840a114cc1e332ac
franciscoRdiaz <francisco.diazbayon@gmail.com>
2019-05-21 18:52:17 +0200

    Connect to remote machine

--- scripts/k8sDeployment.py
+++ scripts/k8sDeployment.py
@@ -7,6 +7,11 @@ from DockerUtils import *
 from checkETM import *
 import time
 import sys
+import urllib3
+import certifi
+import bcrypt
+import PyNaCl
+import paramiko
 
 epm_service_name = "elastest-platform-manager"
 epm_port = "8180"
@@ -43,6 +48,22 @@ def stopEpm(dockerComposeProject):
         dockerComposeProject + " down"
     result = subprocess.call(shlex.split(stopEpmCommand), stderr=FNULL)
 
+def getK8sConfigFromCluster(k8s_host):
+    HOST = k8s_host
+    PUERTO = 22
+    USUARIO = 'ubuntu'
+    datos = dict(hostname=HOST, port=PUERTO, username=USUARIO, key_filename=ansiblePath + '/key')
+
+    ssh_client = paramiko.SSHClient()
+    ssh_client.load_system_host_keys()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(**datos)
+    entrada, salida, error = ssh_client.exec_command('ls -la')
+    print(salida.read())
+    print('--------')
+    entrada, salida, error = ssh_client.exec_command('cat /etc/kubernetes/admin.conf')
+    print(salida.read())
+    ssh_client.close()
 
 def startK8(args, dockerComposeProject):
     if(args.command == 'stop'):
@@ -51,6 +72,7 @@ def startK8(args, dockerComposeProject):
         if(args.paas_ip and args.paas_user and args.paas_pass and args.paas_project_name):
             # TODO check args (start, mini, etc)
             epm_url = startAndWaitForEpm(dockerComposeProject)
+            time.sleep(15)
             # Start EPM
             if(epm_url):
                 # STEP 1: REPLACE HERE WITH THE EPM IP !!!
@@ -93,12 +115,12 @@ def startK8(args, dockerComposeProject):
                 # Send second package for adding a new node
                 resource_group = package_api.receive_package(
                     file=ansiblePath + '/ansible-cluster.tar')
-                print(resource_group)
+                print('Resource group: ', resource_group)
 
-                # This package can contain one VM, which will be added to the cluster
-                resource_group_single = package_api.receive_package(
-                    file=ansiblePath + '/ansible-node.tar')
-                print(resource_group_single)
+                # # This package can contain one VM, which will be added to the cluster
+                # resource_group_single = package_api.receive_package(
+                #     file=ansiblePath + '/ansible-node.tar')
+                # print(resource_group_single)
 
                 # STEP 5: Start the cluster from one of the resource groups
                 cluster_from_resource_group = ClusterFromResourceGroup(
@@ -106,26 +128,29 @@ def startK8(args, dockerComposeProject):
                 cluster = cluster_api.create_cluster(
                     cluster_from_resource_group=cluster_from_resource_group)
 
-                print(cluster)
+                print("Cluster:", cluster)
+                print("K8s_Master:", resource_group.vdus[0].ip)
+                getK8sConfigFromCluster(resource_group.vdus[0].ip)
+                
 
-                # STEP 6: Add a new worker to the Cluster (from the second resource group)
-                cluster_api.add_worker(
-                    id=cluster.id, machine_id=resource_group_single.vdus[0].id)
-
-                print(cluster_api.get_all_clusters())
-
-                # STEP 7: Remove a node from the cluster (Note: This does not remove the VM, just makes the node inactive on the Cluster)
-                cluster_api.remove_node(
-                    id=cluster.id, worker_id=cluster.nodes[0].id)
+                # # STEP 6: Add a new worker to the Cluster (from the second resource group)
+                # cluster_api.add_worker(
+                #     id=cluster.id, machine_id=resource_group_single.vdus[0].id)
 
                 print(cluster_api.get_all_clusters())
 
-                # STEP 8: Remove cluster (Note: does not remove the VMs)
-                cluster_api.delete_cluster(id=cluster.id)
+                # # STEP 7: Remove a node from the cluster (Note: This does not remove the VM, just makes the node inactive on the Cluster)
+                # cluster_api.remove_node(
+                #     id=cluster.id, worker_id=cluster.nodes[0].id)
 
-                # Step 9: Remove OS vms
-                package_api.delete_package(resource_group.id)
-                package_api.delete_package(resource_group_single.id)
+                # print(cluster_api.get_all_clusters())
+
+                # # STEP 8: Remove cluster (Note: does not remove the VMs)
+                # cluster_api.delete_cluster(id=cluster.id)
+
+                # # Step 9: Remove OS vms
+                # package_api.delete_package(resource_group.id)
+                # package_api.delete_package(resource_group_single.id)
         else:
             print('K8 parameters are mandatory')
             exit(1)
